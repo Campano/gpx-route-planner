0c2646a9e63c88bde9b8411513695c72066ad1b2
Simon Campano <scampano@simplicite.fr>
2025-10-15 11:24:16 +0200

    merge

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/gpx-route-planner/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
